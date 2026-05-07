0d71e6dd806cd517a1333cea555b12d86e58f240
Goja <gortnar@beograd.com>
2026-05-07 11:35:16 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
+})->name('index');
+
+Route::get('/hello', function () {
+    return 'Hello';
+})->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect('/hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hallo ' . $name;
+});
+
+
+Route::fallback(function () {
+    return 'No Route';
 });
