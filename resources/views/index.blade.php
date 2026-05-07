2f5e5d3bc44fe474368b01fb11cc4f5960b3a1cd
Goja <gortnar@beograd.com>
2026-05-07 18:34:46 +0200

    Blade direcive again

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,12 @@
 Hallo feom blade template!
 
-@isset($name)
-    The name is {{ $name }}
-@endisset
+<div>
+    @if (count($tasks))
+        <div>There Are tasks!</div>
+        @foreach ($tasks as $task)
+            <li>{{ $task->title }}</li>
+        @endforeach
+    @else
+        <div>There are no tasks!</div>
+    @endif
+</div>
